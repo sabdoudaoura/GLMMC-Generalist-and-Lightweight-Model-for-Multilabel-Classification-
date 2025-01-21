de6cc3330c8d0938791cac63201cb27911245861
pirate0007 <adsalim007@gmail.com>
2025-01-21 23:02:22 +0100

    update

--- dataset.py
+++ dataset.py
@@ -1,41 +1,57 @@
 import random
+import torch
 import json
 import os
 from torch.utils.data import Dataset#, DataLoader
+from transformers import AutoTokenizer
 import yaml
 
-# Load the configuration file
-with open("config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+# # Load the configuration file
+# with open("config.yaml", "r") as file:
+#     config = yaml.safe_load(file)
 
 # Access parameters
-data_path = config["data"]["synthetic_data_path"]
+# data_path = config["data"]["synthetic_data_path"]
 
 
 
 class CustomDataset(Dataset):
-    def __init__(self, data_path, transform=None):
-        self.data_path = data_path
-        self.data, self.labels = self.preprocessing()
-        self.transform = transform
+    def __init__(self, data, model_name, max_num_labels, transform=None, max_num_negatives=2):
 
-    def preprocessing(self):
         # Open and read json
-        with open(self.data_path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
+        self.data = data
+        self.max_num_negatives = max_num_negatives
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.transform = transform
+        self.all_labels = list(set([label for labels in [element["labels"] for element in data] for label in labels]))
+        self.max_num_labels = max_num_labels
+    # def encode(self):
+          
         
-        texts = [entry["text"] for entry in data]
-        labels = [entry["labels"] for entry in data]
-        return texts, labels
+    #     texts = [self.tokenizer(entry["text"], return_tensors='pt', padding=True, truncation=True) for entry in data]
+    #     labels = [self.tokenizer(entry["labels"], return_tensors='pt', padding=True, truncation=True) for entry in data]
+    #     return texts, labels
     
     def __getitem__(self, index):
-        return self.data[index], self.labels[index]
-    
+
+
+      adversarial_example = self.negative_sampling([self.data[index]['labels']], self.all_labels)
+      self.texts = self.tokenizer(self.data[index]['text'], return_tensors='pt', padding='max_length', truncation=True, max_length=15)
+      self.labels = self.tokenizer(self.data[index]['labels'] + adversarial_example[0], return_tensors='pt', padding='max_length', truncation=True, max_length=5)
+ 
+      p = len(self.data[index]['labels'])
+      q = len(adversarial_example[0])
+      
+      #trouble if max_num_labels < (p+q)
+      target_labels = torch.cat([torch.ones(p), torch.zeros(q), torch.zeros(self.max_num_labels -(p+q))]) #Ensures that scores and target are the same length
+
+      return self.texts, self.labels, p+q, target_labels
+
     def __len__(self):
         return len(self.data)
 
-    def negative_sampling(batch_labels, all_labels, max_num_negatives=2):
-        num_negatives = random.randint(1, max_num_negatives)
+    def negative_sampling(self, batch_labels, all_labels):
+        num_negatives = random.randint(1, self.max_num_negatives)
         negative_samples = []
         for labels in batch_labels:
             neg = random.sample([l for l in all_labels if l not in labels], num_negatives)
@@ -45,5 +61,7 @@ class CustomDataset(Dataset):
 
 # Example Usage
 if __name__ == "__main__":
-    custom_dataset = CustomDataset(data_path)
-    print(custom_dataset.__getitem__(5))
+
+    # custom_dataset = CustomDataset(data_path)
+    # print(custom_dataset.__getitem__(5))
+    print("Hello world")
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
-import torch 
+import torch
 from torch import nn
+import json
 import yaml
 from model import BiEncoderModel
 from torch.optim import AdamW
@@ -7,18 +8,19 @@ from dataset import CustomDataset
 from torch.utils.data import DataLoader
 
 # Load the configuration file
-with open("config.yaml", "r") as file:
+with open("/content/drive/MyDrive/projet classification model*/config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 # Access parameters
-data_path = config["data"]["synthetic_data_path"]
+#data_path = config["data"]["synthetic_data_path"]
+data_path = "/content/drive/MyDrive/projet classification model*/data/synthetic_data.json"
 model_name = config["model"]["name"]
 max_num_labels = config["model"]["max_num_labels"]
-learning_rate = 0.0002#config["training"]["learning_rate"]
+learning_rate = float(config["training"]["learning_rate"])
 batch_size = config["training"]["batch_size"]
 # Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-epochs = 10
+epochs = config["training"]["epochs"]
 
 
 # Custom collate function
@@ -26,7 +28,9 @@ def custom_collate_fn(batch):
     # Separate texts and labels from the batch
     texts = [item[0] for item in batch]
     labels = [item[1] for item in batch]
-    return texts, labels
+    num_labels = [item[2] for item in batch]
+    target_labels = [item[3] for item in batch]
+    return texts, labels, num_labels, target_labels
 
 
 
@@ -34,61 +38,70 @@ if __name__ == "__main__":
 
 
 
-    model = BiEncoderModel(model_name, max_num_labels)
+    model = BiEncoderModel(model_name, max_num_labels).to(device)
+
+    # #Freeze base model parameters
+    # for param in model.parameters():
+    #   param.requires_grad = False
+
+    # Unfreeze the pooler layer to have a better representation for our classification task
+    # for param in model.shared_encoder.pooler.parameters():
+    #   param.requires_grad = True
+
+
     criterion = nn.BCEWithLogitsLoss() # multiclass classification
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
 
-    model.to(device)
+    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = AdamW(trainable_params, lr=learning_rate)
 
+    with open(data_path, 'r', encoding='utf-8') as f:
+      data = json.load(f)
 
     # Training loop
-    dataset = CustomDataset(data_path)
+    dataset = CustomDataset(data, model_name, max_num_labels)
+
     # Dataloader
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=custom_collate_fn)
 
-    
-    # for batch_idx, (texts, batch_labels) in enumerate(train_loader):
-    #     print(f"{len(texts)}, {len(batch_labels)}")
-
-
-    
-    
     for epoch in range(epochs):
         model.train()  # Set the model to training mode
         running_loss = 0.0
 
-        for batch_idx, (texts, batch_labels) in enumerate(train_loader):
-            print(len(texts))
-            print(len(batch_labels))
-            texts = [text for text in texts]  # Make sure text is in a list
-            batch_labels = batch_labels  # Labels are in list of lists
+        for batch_idx, (texts, batch_labels, num_labels, target_labels) in enumerate(train_loader):
 
-            # # load to device (CPU ou GPU) # useless as gpu only uses numbers
-            # texts = [text.to(device) for text in texts]
-            # batch_labels = [label.to(device) for label in batch_labels]
+            input_ids_text = torch.stack([item['input_ids'] for item in texts]).squeeze(1).to(device)
+            attention_mask_text = torch.stack([item['attention_mask'] for item in texts]).squeeze(1).to(device)
 
-            # Forward pass
+            # token_type_ids_text = torch.stack([item['token_type_ids'] for item in texts]).squeeze(1)
+
+            input_ids_labels = torch.cat([item['input_ids'] for item in batch_labels], dim=0).to(device)
+            attention_mask_labels = torch.cat([item['attention_mask'] for item in batch_labels], dim=0).to(device)
+
+            num_labels = torch.tensor(num_labels).to(device)
+            target_labels = torch.stack(target_labels, dim=0).to(device)
+
+            input_ids_text.to(device)
+            attention_mask_text.to(device)
+            input_ids_labels.to(device)
+            attention_mask_labels.to(device)
+
+            #Forward pass
             optimizer.zero_grad()
-            scores, mask = model.forward(texts, batch_labels)  # Scores: [B, max_num_labels], mask: [B, max_num_labels]
+            scores, mask = model.forward(input_ids_text, attention_mask_text, input_ids_labels, attention_mask_labels, num_labels)  # Scores: [B, max_num_labels], mask: [B, max_num_labels]
+            preds = torch.sigmoid(scores)
 
-            # Création de la target pour le calcul de la perte
-            targets = torch.zeros_like(scores)
-            for i, labels in enumerate(batch_labels):
-                for j, label in enumerate(labels):
-                    # Marquer la cible à 1 pour les labels présents
-                    targets[i, j] = 1.0
 
-            # Calcul de la perte
-            loss = criterion(scores, targets)
-            loss = (loss * mask).sum() / mask.sum()  # Appliquer le masque pour exclure les labels non présents dans chaque texte
+            # Compute the loss
+            scores = scores * mask #Apply mask to put to 0 all not used elements
+            loss = criterion(scores, target_labels)
 
-            # Backward pass et mise à jour des poids
+            # Backward pass and update weights
+            #loss.requires_grad = True
             loss.backward()
             optimizer.step()
 
             # Log de la perte
             running_loss += loss.item()
 
-            if (batch_idx + 1) % 10 == 0:  # Afficher les pertes toutes les 10 itérations
+            if (batch_idx + 1) % 5 == 0:  # Afficher les pertes toutes les 10 itérations
                 print(f"Epoch [{epoch + 1}/{epochs}], Step [{batch_idx + 1}/{len(train_loader)}], Loss: {running_loss / (batch_idx + 1):.4f}")
-
